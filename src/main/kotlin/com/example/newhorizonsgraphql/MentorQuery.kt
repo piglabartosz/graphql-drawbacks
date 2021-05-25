f3511252420bd9680d818fe6ec5e69edd7268420
Bartosz Pigla <piglabartosz@gmail.com>
2021-05-25 13:45:08 +0200

    Add custom scalar configuration

--- src/main/kotlin/com/example/newhorizonsgraphql/MentorQuery.kt
+++ src/main/kotlin/com/example/newhorizonsgraphql/MentorQuery.kt
@@ -2,10 +2,27 @@ package com.example.newhorizonsgraphql
 
 import com.expediagroup.graphql.server.operations.Query
 import org.springframework.stereotype.Component
+import java.time.Instant
 
 @Component
 class MentorQuery : Query {
-    fun mentor() = Mentor("John")
+    fun mentor() = Mentor(
+        name = "John",
+        sessions = listOf(
+            Session(
+                time = Instant.now(),
+                secretData = "secret"
+            )
+        )
+    )
 }
 
-data class Mentor(val name: String)
+data class Mentor(
+    val name: String,
+    val sessions: List<Session>
+)
+
+data class Session(
+    val time: Instant,
+    val secretData: String
+)
